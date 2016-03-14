37604375192dcaf6e38ba2ebbdf2e0ecc152b6e2
migdall <jesus.noland@litpen.com>
2016-03-14 14:14:24 -0700

    Adding more class doc notes to Card. Adding get methods for name and type.

--- cards/models.py
+++ cards/models.py
@@ -1,8 +1,16 @@
 # Models for Cards package
 
 class Card:
-    """ A simple Card class """
+    """A simple Card class\n
+    Initialize a Card object with the __init__ method.\n"""
 
     def __init__(self, name, type):
+        # Instance properties
         self.name = name
         self.type = type
+
+    def get_name(self):
+        return self.name
+
+    def get_type(self):
+        return self.type
