c2c3e3d66219d7728a197a131bdc48306f5d8a91
migdall <jesus.noland@litpen.com>
2016-03-15 09:28:10 -0700

    Implementing the PooCard class derived from class Card. Adding the repr and str methods to the base class Card.

--- cards/models.py
+++ cards/models.py
@@ -9,8 +9,26 @@ class Card:
         self.name = name
         self.type = type
 
+    def __repr__(self):
+        return "%s Card of type %s" % (self.name, self.type)
+
+    def __str__(self):
+        return "%s Card" % self.name
+
     def get_name(self):
         return self.name
 
     def get_type(self):
         return self.type
+
+class PooCard (Card):
+    """ Poo Card class that inherits from the class Card\n
+    Initialize a PooCard object with the __init__ method.\n"""
+
+    def __init__(self, name, damage):
+        # Initialize the Card properties
+        Card.__init__(self, name, "POO")
+        self.damage = damage
+
+    def get_damage(self):
+        return self.damage
